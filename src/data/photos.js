cd9dcca229abf8d34c61890be8590e5c37ebd74a
Hariom Thakur <HARIKRDBG121@GMAIL.COM>
2026-07-03 16:41:58 +0530

    Update photos.js

--- src/data/photos.js
+++ src/data/photos.js
@@ -19,9 +19,9 @@ export const photos = [
   { id: 1, src: '/photos/photo1.png', caption: 'Beautiful Shagun! 💕', span: 2 },
   { id: 2, src: '/photos/photo2.png', caption: 'Smiling always 🌸', span: 1 },
   { id: 3, src: '/photos/photo3.png', caption: 'Sunshine girl ✨', span: 1 },
-  { id: 4, src: '/photos/photo4.png', caption: 'Shagun vibes 🦋', span: 2 },
-  { id: 5, src: '/photos/photo5.png', caption: 'Happy birthday! 🎂', span: 1 },
-  { id: 6, src: '/photos/photo6.png', caption: 'Forever glowing 🌟', span: 1 },
+  { id: 4, src: '/photos/photo4.png', caption: 'Special vibes 🦋', span: 2 },
+  { id: 5, src: '/photos/photo5.png', caption: 'Forever glowing 🌟', span: 1 },
+  { id: 6, src: '/photos/photo6.png', caption: 'Happy birthday! 🎂', span: 1 },
   { id: 7, src: '/photos/photo7.png', caption: 'Golden girl 💛', span: 1 },
-  { id: 8, src: '/photos/photo8.png', caption: 'Spreading love 💗', span: 2 },
+  { id: 8, src: '/photos/photo8.png', caption: 'Spreading beauty 💗', span: 2 },
 ];
